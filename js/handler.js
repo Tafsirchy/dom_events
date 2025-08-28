b71762d3d16032743d4bbb2dc4face1c7b5b603e
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-28 21:50:07 +0600

    25-4 Integrate Event innerText and Text update by Event Event Handlers

---
+++ js/handler.js
@@ -0,0 +1,13 @@
+document.getElementById("btn-update-title").addEventListener("click", function () {
+        //   console.log("Button clicked");
+        const pageTitleElement = document.getElementById("page-title");
+        //   console.log(pageTitleElement);
+        pageTitleElement.innerText = "Updated Page Tile Element";
+    });
+
+
+document.getElementById("btn-update-name").addEventListener("click", function () {
+        const btnUpdateName = document.getElementById("name");
+
+        btnUpdateName.innerText = "M Tafsirul Islam Chowdhury";
+    });
